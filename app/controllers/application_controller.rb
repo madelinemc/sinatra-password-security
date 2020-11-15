b6938399caf009a7a4fd6e098063678c69c4a1e2
madelinemc <madeline.coons@gmail.com>
2020-11-15 11:07:56 -0600

    commit tree 0ec2228578918bf6391eb2e6b00a58143acb0517

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,27 +8,42 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
-	get "/" do
+	get "/" do #renders the index.erb file which has links to sign up or login
 		erb :index
 	end
 
-	get "/signup" do
+	get "/signup" do #renders a form to create a new user with form fields username and password
 		erb :signup
 	end
 
-	post "/signup" do
-		#your code here!
-	end
+	post "/signup" do #makes a new instance of the user class with a username and password form params
+		user = User.new(:username => params[:username], :password => params[:password]) 
+		#Note that even though our database has a column called password_digest, we still access the attribute of password. This is given to us by has_secure_password.
 
-	get "/login" do
+		if user.save #will not be able to save to database if the password field is NOT filled out. user.save will return false if the user can't be persisted so redirect to login if true and failure page if false.
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
+	end 
+
+	get "/login" do #renders a form for logging in
 		erb :login
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username]) #find user by username
+		if user && user.authenticate(params[:password])
+			#check to confirm if a user was found. can be written as user or user != nil 
+			#also, check to confirm user's password matches up the value in password_digest by authenticate method provided by has_secure_password macro. Ruby handles this "invisibly" for us. 
+			session[:user_id] = user.id #if user authenticates, the session's user_id is set to that user's id. 
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
 	end
 
-	get "/success" do
+	get "/success" do #renders a success.erb file which is displayed once a user successfully logs in
 		if logged_in?
 			erb :success
 		else
@@ -36,21 +51,21 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
-	get "/failure" do
+	get "/failure" do #renders a failure.erb page which will be accessed if there is an error logging in or signing up. 
 		erb :failure
 	end
 
-	get "/logout" do
+	get "/logout" do #clears the session data and redirects to homepage
 		session.clear
 		redirect "/"
 	end
 
-	helpers do
+	helpers do # renders a true or false based on the presence of a session[:user_id]
 		def logged_in?
 			!!session[:user_id]
 		end
 
-		def current_user
+		def current_user #returns the instance of the logged in user, based on the session[:user_id]
 			User.find(session[:user_id])
 		end
 	end
--- db/migrate/20201115165854_update_table_users.rb
+++ db/migrate/20201115165854_update_table_users.rb
@@ -1,14 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  
-  def up
+class UpdateTableUsers < ActiveRecord::Migration[5.1]
+
+  def change
     create_table :users do |t|
       t.string :username
       t.string :password_digest
     end
   end
-
-  def down
-    drop_table :users
-  end
-
+  
 end
